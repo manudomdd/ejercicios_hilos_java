8f361000dcd56490c9bb7d8caab2adcef5bbf64c
Manu <dominguez.dieguez.manuel.jesus@iescamas.es>
2025-10-26 03:56:05 +0100

    EjercicioRunnable

--- src/ejercicios_hilos_java/LanzadorHilosNew.java
+++ src/ejercicios_hilos_java/LanzadorHilosNew.java
@@ -2,8 +2,14 @@ package ejercicios_hilos_java;
 
 public class LanzadorHilosNew {
 	
-	public static void main (String[] args) {
-		Thread h = new Thread(new Hilo("nuevo")); 
-		System.out.println(h.getName() + " --->" + h.getState()); 
+	public static void main (String[] args) throws InterruptedException {
+		Thread h = new Thread(new Hilo("a")); 
+		h.start();
+		try {
+			System.out.println(h.getName() + " ---> " + h.getState());
+			Thread.sleep(10);		
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}		 
 	}
 }
